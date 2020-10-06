e64e24cba357322e883761b5c48aab559ffeaa67
ginajonas <ginajonas7@gmail.com>
2020-10-06 13:00:51 -0400

    Adding Typewriter
    Adding Typewriter

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ window.addEventListener('scroll', function(){
     road.style.top = value * 0.15 + 'px';
     text.style.top = value * 1 + 'px';
 })
+
+let i = 0;
+let about = "I'm a dedicated mother of a wonderful 4 year old boy. I love spending time with him and my lovely better half. I'm passionate and enjoy the many challenges that life brings along. I'm a front-end web developer leveraging healthcare and beauty and arts background to build a more intuitive user experience on the web. Currently earning a certificate in full stack development from Carleton University in Ottawa, with newly developed skills in JavaScript, CSS, and responsive web design. Known as an innovative problem solver passionate about developing apps, with a focus on mobile-first design and development. With each project, my aim is to best engage my audience for an impactful user experience. I’m excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web."
+let characters = about.split("")
+setInterval(function(){
+    $(".anim-typewriter").append(characters[i])
+    i++
+    if (i=== characters.length) {
+       clearInterval(this)
+    }
+} ,50)
